5508102cfc20999a1bd281d77f5b1df28182a64a
skotur1979 <prvostupnik@gmail.com>
2026-03-18 09:57:01 +0100

    Machines

--- app/Services/WasteTrackingPdfGenerator.php
+++ app/Services/WasteTrackingPdfGenerator.php
@@ -223,21 +223,21 @@ class WasteTrackingPdfGenerator
 
         $hpY = 40.0;
         $hpXs = [
-            'HP1'  => 38,
-            'HP2'  => 47.8,
+            'HP1'  => 38.1,
+            'HP2'  => 48.5,
             'HP3'  => 58.5,
             'HP4'  => 68.8,
             'HP5'  => 79.3,
-            'HP6'  => 86.2,
+            'HP6'  => 90.0,
             'HP7'  => 100.0,
-            'HP8'  => 108.0,
-            'HP9'  => 115.9,
-            'HP10' => 130.0,
-            'HP11' => 142.6,
-            'HP12' => 152.7,
-            'HP13' => 160.8,
-            'HP14' => 180.0,
-            'HP15' => 189.2,
+            'HP8'  => 110.0,
+            'HP9'  => 120.9,
+            'HP10' => 132.0,
+            'HP11' => 144.0,
+            'HP12' => 155.0,
+            'HP13' => 166.0,
+            'HP14' => 178.0,
+            'HP15' => 189.3,
         ];
 
         foreach ($hpXs as $hp => $x) {
@@ -270,15 +270,15 @@ class WasteTrackingPdfGenerator
 
         $packY = 48.8;
         $packXs = [
-            'rasuto'    => 42.0,
-            'posude'    => 56.5,
+            'rasuto'    => 43.0,
+            'posude'    => 57.0,
             'kanta'     => 69.8,
-            'kutija'    => 144.0,
+            'kutija'    => 128.0,
             'kanister'  => 85.0,
-            'kontejner' => 113.0,
-            'bacva'     => 132.0,
-            'vreca'     => 140.0,
-            'ostalo'    => 153.0,
+            'kontejner' => 100.0,
+            'bacva'     => 115.0,
+            'vreca'     => 135.0,
+            'ostalo'    => 151.0,
         ];
 
         foreach ($packXs as $pack => $x) {
@@ -292,7 +292,7 @@ class WasteTrackingPdfGenerator
         $writeLine(176.8, 48.0, 14, $record->package_count, 7, 'B', 'C');
 
         $multiline(20.0, 52.1, 170, $record->waste_description, 10);
-        $multiline(120.0, 66.0, 60, $record->municipal_origin_note, 10);
+        $multiline(110.0, 67.0, 60, $record->municipal_origin_note, 10);
 
         /*
         |--------------------------------------------------------------------------
@@ -300,11 +300,11 @@ class WasteTrackingPdfGenerator
         |--------------------------------------------------------------------------
         */
 
-        $writeLine(15.0, 79.5, 90, $record->sender_person_name, 9);
-        $writeLine(20.0, 85.2, 90, $record->sender_oib, 9);
-        $writeLine(32.0, 90.5, 90, $record->sender_nkd_code, 9);
-        $writeLine(30.0, 96.0, 90, $record->sender_contact_person, 9);
-        $multiline(30.0, 101.6, 90, $record->sender_contact_data, 9);
+        $writeLine(15.0,79.8, 90, $record->sender_person_name, 9);
+        $writeLine(18.0, 85.2, 90, $record->sender_oib, 9);
+        $writeLine(32.0, 90.8, 90, $record->sender_nkd_code, 9);
+        $writeLine(29.7, 96.2, 90, $record->sender_contact_person, 9);
+        $multiline(30.0, 101.7, 90, $record->sender_contact_data, 9);
 
         /*
         |--------------------------------------------------------------------------
@@ -312,23 +312,23 @@ class WasteTrackingPdfGenerator
         |--------------------------------------------------------------------------
         */
 
-        $writeLine(112.2, 80.8, 83, $record->waste_owner_at_handover, 9);
+        //$writeLine(112.2, 80.8, 83, $record->waste_owner_at_handover, 9);
 
-        $box($record->report_choice === 'da', 135.0, 80.8);
-        $box($record->report_choice === 'ne', 145.0, 80.8);
-        $box($record->purpose_choice === 'oporaba', 177.5, 85.8);
-        $box($record->purpose_choice === 'zbrinjavanje', 196.4, 85.8);
+        $box($record->report_choice === 'da', 147.0, 81);
+        $box($record->report_choice === 'ne', 154.0, 81);
+        $box($record->purpose_choice === 'oporaba', 139, 87);
+        $box($record->purpose_choice === 'zbrinjavanje', 153, 87);
 
-        $writeLine(112.2, 91.2, 83, $record->dispatch_point, 7);
-        $writeLine(112.2, 96.4, 83, $record->destination_point, 7);
-        $writeLine(144.0, 101.5, 16, $fmtNum($record->quantity_m3, 3), 7, '', 'R');
-        $writeLine(169.8, 101.5, 16, $fmtNum($record->quantity_kg, 2), 7, '', 'R');
+        $writeLine(119, 92.5, 83, $record->dispatch_point, 9);
+        $writeLine(119, 97.9, 83, $record->destination_point, 9);
+        $writeLine(113.8, 103.8, 16, $fmtNum($record->quantity_m3, 3), 9, '', 'R');
+        $writeLine(133.7, 103.8, 16, $fmtNum($record->quantity_kg, 2), 9, '', 'R');
 
-        $box($record->quantity_determination_choice === 'vaganje', 187.8, 101.2);
-        $box($record->quantity_determination_choice === 'procjena', 200.2, 101.2);
+        $box($record->quantity_determination_choice === 'vaganje', 174, 104.5);
+        $box($record->quantity_determination_choice === 'procjena', 185, 104.5);
 
-        $writeLine(112.2, 106.7, 40, $fmtDateTime($record->handover_datetime), 7);
-        $writeLine(112.2, 111.9, 83, $record->handed_over_by, 7);
+        $writeLine(126, 109.5, 40, $fmtDateTime($record->handover_datetime), 9);
+        $writeLine(114.8, 115, 83, $record->handed_over_by, 9);
 
         /*
         |--------------------------------------------------------------------------
@@ -336,22 +336,22 @@ class WasteTrackingPdfGenerator
         |--------------------------------------------------------------------------
         */
 
-        $writeLine(7.8, 129.6, 90, $record->carrier_name, 7);
-        $writeLine(7.8, 134.8, 90, $record->carrier_oib, 7);
-        $writeLine(7.8, 140.0, 90, $record->carrier_authorization, 7);
-        $writeLine(7.8, 145.2, 90, $record->carrier_contact_person, 7);
-        $multiline(7.8, 150.2, 90, $record->carrier_contact_data, 7);
+        $writeLine(15, 126.3, 90, $record->carrier_name, 8);
+        $writeLine(12, 131.9, 90, $record->carrier_oib, 8);
+        $writeLine(35, 137.4, 90, $record->carrier_authorization, 8);
+        $writeLine(30, 142.9, 90, $record->carrier_contact_person, 8);
+        $multiline(30, 148.5, 90, $record->carrier_contact_data, 8);
 
-        $box($has($record->transport_modes, 'cestovni'), 170.0, 129.1);
-        $box($has($record->transport_modes, 'zeljeznicki'), 181.5, 129.1);
-        $box($has($record->transport_modes, 'morski'), 193.1, 129.1);
-        $box($has($record->transport_modes, 'zracni'), 170.0, 134.0);
-        $box($has($record->transport_modes, 'unutarnji_plovni_put'), 193.1, 134.0);
+        $box($has($record->transport_modes, 'cestovni'), 159.0, 122.1);
+        $box($has($record->transport_modes, 'zeljeznicki'), 176.0, 122.1);
+        $box($has($record->transport_modes, 'morski'), 190, 122.1);
+        $box($has($record->transport_modes, 'zracni'), 158.5, 126.8);
+        $box($has($record->transport_modes, 'unutarnji_plovni_put'), 193.8, 126.8);
 
-        $writeLine(112.2, 140.0, 83, $record->carrier_vehicle_registration, 7);
-        $writeLine(112.2, 145.2, 83, $record->carrier_taken_over_by, 7);
-        $writeLine(112.2, 150.4, 40, $fmtDateTime($record->carrier_taken_over_at), 7);
-        $writeLine(112.2, 155.6, 83, $record->carrier_delivered_by, 7);
+        $writeLine(136,132.0, 83, $record->carrier_vehicle_registration, 8);
+        $writeLine(120.2, 137, 83, $record->carrier_taken_over_by, 8);
+        $writeLine(127, 142.2, 40, $fmtDateTime($record->carrier_taken_over_at), 8);
+        $writeLine(118.2,148, 83, $record->carrier_delivered_by, 8);
 
         /*
         |--------------------------------------------------------------------------
@@ -359,15 +359,15 @@ class WasteTrackingPdfGenerator
         |--------------------------------------------------------------------------
         */
 
-        $writeLine(7.8, 172.8, 90, $record->receiver_name, 7);
-        $writeLine(7.8, 178.0, 90, $record->receiver_oib, 7);
-        $writeLine(7.8, 183.2, 90, $record->receiver_authorization, 7);
-        $writeLine(7.8, 188.4, 90, $record->receiver_contact_person, 7);
-        $multiline(7.8, 193.4, 90, $record->receiver_contact_data, 7);
+        $writeLine(15, 159.5, 90, $record->receiver_name, 8);
+        $writeLine(12, 164, 90, $record->receiver_oib, 8);
+        $writeLine(41, 169, 90, $record->receiver_authorization, 8);
+        $writeLine(30, 174, 90, $record->receiver_contact_person, 8);
+        $multiline(30, 180, 90, $record->receiver_contact_data, 8);
 
-        $writeLine(112.2, 178.0, 83, $record->receiver_taken_over_by, 7);
-        $writeLine(112.2, 188.4, 40, $fmtDateTime($record->receiver_weighing_time), 7);
-        $writeLine(145.0, 193.6, 20, $fmtNum($record->receiver_measured_quantity_kg, 2), 7, '', 'R');
+        $writeLine(170.2, 161.0, 83, $record->receiver_taken_over_by, 8);
+        $writeLine(130, 172.5, 40, $fmtDateTime($record->receiver_weighing_time), 8);
+        $writeLine(133.0, 178, 20, $fmtNum($record->receiver_measured_quantity_kg, 2), 8, '', 'R');
 
         /*
         |--------------------------------------------------------------------------
@@ -375,11 +375,11 @@ class WasteTrackingPdfGenerator
         |--------------------------------------------------------------------------
         */
 
-        $writeLine(7.8, 219.8, 90, $record->trader_name, 7);
-        $writeLine(7.8, 225.0, 90, $record->trader_oib, 7);
-        $writeLine(7.8, 230.2, 90, $record->trader_authorization, 7);
-        $writeLine(7.8, 235.4, 90, $record->trader_contact_person, 7);
-        $multiline(7.8, 240.4, 90, $record->trader_contact_data, 7);
+        $writeLine(15, 192, 90, $record->trader_name, 8);
+        $writeLine(12, 197.2, 90, $record->trader_oib, 8);
+        $writeLine(20, 202.5, 90, $record->trader_authorization, 8);
+        $writeLine(30, 208, 90, $record->trader_contact_person, 8);
+        $multiline(30, 213, 90, $record->trader_contact_data, 8);
 
         /*
         |--------------------------------------------------------------------------
@@ -387,12 +387,12 @@ class WasteTrackingPdfGenerator
         |--------------------------------------------------------------------------
         */
 
-        $writeLine(112.2, 219.8, 83, $record->processor_name, 7);
-        $writeLine(112.2, 225.0, 83, $record->processor_oib, 7);
-        $writeLine(112.2, 230.2, 83, $record->processor_authorization, 7);
-        $writeLine(112.2, 235.4, 40, $fmtDate($record->processing_completed_at), 7);
-        $writeLine(112.2, 240.6, 83, $record->final_processing_method, 7);
-        $writeLine(112.2, 245.8, 83, $record->processor_confirmed_by, 7);
+        $writeLine(113, 192, 83, $record->processor_name, 8);
+        $writeLine(111.5, 198, 83, $record->processor_oib, 8);
+        $writeLine(130, 203, 83, $record->processor_authorization, 8);
+        $writeLine(136, 209, 40, $fmtDate($record->processing_completed_at), 8);
+        $writeLine(130, 214, 83, $record->final_processing_method, 8);
+        $writeLine(175, 219, 83, $record->processor_confirmed_by, 8);
 
         /*
         |--------------------------------------------------------------------------
@@ -400,7 +400,7 @@ class WasteTrackingPdfGenerator
         |--------------------------------------------------------------------------
         */
 
-        $multiline(7.8, 255.8, 194, $record->note, 7);
+        $multiline(7.8, 229, 194, $record->note, 8);
 
         $attachments = collect($toArray($record->attachments))
             ->map(fn ($file) => basename((string) $file))
