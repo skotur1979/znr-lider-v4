2a564cafbabe3abe3c8eb749a2764f778392e482
Domagoj Samardžić <domagojsamardzic123@gmail.com>
2026-07-24 20:28:20 +0200

    Profil uređen

--- app/Filament/Pages/Auth/EditProfile.php
+++ app/Filament/Pages/Auth/EditProfile.php
@@ -4,9 +4,34 @@ namespace App\Filament\Pages\Auth;
 
 use Filament\Actions\Action;
 use Filament\Auth\Pages\EditProfile as BaseEditProfile;
+use Filament\Schemas\Components\Utilities\Get;
+use Filament\Schemas\Schema;
 
 class EditProfile extends BaseEditProfile
 {
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                $this->getNameFormComponent()
+                    ->label('Ime'),
+
+                $this->getEmailFormComponent()
+                    ->label('Adresa e-pošte'),
+
+                $this->getPasswordFormComponent()
+                    ->label('Nova lozinka')
+                    ->helperText('Ostavite prazno ako ne želite promijeniti lozinku.')
+                    ->live(),
+
+                $this->getPasswordConfirmationFormComponent()
+                    ->label('Potvrdi novu lozinku')
+                    ->visible(
+                        fn (Get $get): bool => filled($get('password'))
+                    ),
+            ]);
+    }
+
     protected function getCancelFormAction(): Action
     {
         return Action::make('cancel')
